5562e6f3a7499d7fd4d101c206bf8bee573826f0
FloreaHorea <37344652+FloreaHorea@users.noreply.github.com>
2018-04-04 01:20:33 +0300

    addnew

---
+++ addnew.js
@@ -0,0 +1,7 @@
+console.log('hello there')
+console.log('hello there', 'gordon')
+var todos = ['item 1', 'item 2', 'item 3']
+console.log(todos)
+console.log('My todos',todos)
+todos.push('item 4')
+todos.push('item 5')
